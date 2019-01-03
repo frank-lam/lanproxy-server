105386fd84f9b6f47148cefbd5b8d67c73aae190
root <1002039945@qq.com>
2019-01-03 13:23:39 +0800

    修改配置文件

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i "s|input_your_key|$LANPROXY_USER|g" /lanproxy-client/distribution/proxy-server-0.1/conf/config.properties
-sed -i "s|input_your_host|$LANPROXY_PWD|g" /lanproxy-client/distribution/proxy-server-0.1/conf/config.properties
+sed -i "s|input_your_key|$LANPROXY_USER|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
+sed -i "s|input_your_host|$LANPROXY_PWD|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
 
 # starting 
 sh /lanproxy-server/start.sh
